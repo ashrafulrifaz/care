cdc4fa228521353cce876c3a7e3c07eae338599d
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-11 18:42:53 +0600

    project setup completed

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+const uri = process.env.MONGODB_URI
+const dbName = process.env.DBNAME
+export const collections = {
+    USERS: 'users',
+    BOOKINGS: 'bookings'
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export const dbConnect = (cname) => {
+    return client.db(dbName).collection(cname)
+}
