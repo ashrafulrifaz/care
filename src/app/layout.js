e70f6d4d3f0f3725e4ba2bead3777b16fb58bf17
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-11 18:49:01 +0600

    added google fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Manrope, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const manrope = Manrope({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin']
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const nunitoSans = Nunito_Sans({
+  weight: ['400' ,'500', '600', '700'],
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: "Care - Your Personal Health Assistant",
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manrope.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { nunitoSans } from "./layout";
 
 export default function Home() {
   return (
     <div>
       home
+      <h3 className={`${nunitoSans.className}`}>nunito sans</h3>
     </div>
   );
 }
