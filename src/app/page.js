2329f251edbef002ea4d83b0ac1b0b725bcbd8c1
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-11 22:06:10 +0600

    added service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import About from "@/components/HomeComponents/About";
 import Hero from "@/components/HomeComponents/Hero";
+import Services from "@/components/HomeComponents/Services";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <Services />
     </div>
   );
 }
