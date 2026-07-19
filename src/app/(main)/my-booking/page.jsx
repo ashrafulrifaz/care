40a799ea6d3f6fab4e28048876b62ff70cf995d3
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-19 12:23:51 +0600

    added my bookig page

---
+++ src/app/(main)/my-booking/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            ha ha ha
+        </div>
+    );
+};
+
+export default page;
--- src/components/Header/NavItems.jsx
+++ src/components/Header/NavItems.jsx
@@ -1,17 +1,22 @@
 'use client'
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const NavItems = () => {
     const path = usePathname()
+    const session = useSession()
 
     return (
         <div className="flex items-center justify-center gap-4">
             <Link href={'/'} className={`capitalize ${path === '/' ? 'font-semibold' : 'font-medium'}`}>Home</Link>
             <Link href={'/services'} className={`capitalize ${path === '/services' ? 'font-semibold' : 'font-medium'}`}>services</Link>
             <Link href={'/about-us'} className={`capitalize ${path === '/about-us' ? 'font-semibold' : 'font-medium'}`}>about us</Link>
-            <Link href={'/booking'} className={`capitalize ${path === '/booking' ? 'font-semibold' : 'font-medium'}`}>booking</Link>
+            {
+                session.status == 'authenticated' ? 
+                <Link href={'/my-booking'} className={`capitalize ${path === '/my-booking' ? 'font-semibold' : 'font-medium'}`}>my booking</Link> : ''
+            }
         </div>
     );
 };
